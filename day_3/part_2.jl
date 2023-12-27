2690e3a29cf25090438f9f731322218cec27ac27
mbarbattini <mbarbattini@gmail.com>
2023-12-26 20:01:39 -0700

    day 3 part 2 complete

--- day_3/part_2.jl
+++ day_3/part_2.jl
@@ -76,84 +76,99 @@ function read_number(grid, i, j)
 end
 
 
-function solve_part_1()
+function solve_part_2()
 
     total = 0
 
     for i in 1:N_DIM_1
         for j in 1:N_DIM_2
             current_char = grid[i, j]
-            if current_char in symbols
+            if current_char == '*'
+                match_count = 0
+                tentative_digit_list = []
                 if i > 1 && j > 1
                     upper_left = grid[i-1, j-1]
                 end
                 if upper_left in digits
-                    num = read_number(grid, i-1, j-1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i-1, j-1))
+                    # print("$num\n")
+                    # total += num
                 end
                 # because grid gets cleared in read_number, need to get the entry again
                 if i > 1
                     top = grid[i-1, j]
                 end
                 if top in digits
-                    num = read_number(grid, i-1, j)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i-1, j) )
+                    # print("$num\n")
+                    # total += num
                 end 
                 if i > 1 && j < N_DIM_2
                     upper_right = grid[i-1, j+1]
                 end
                 if upper_right in digits
-                    num = read_number(grid, i-1, j+1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i-1, j+1))
+                    # print("$num\n")
+                    # total += num
                 end
                 if j > 1
                    left = grid[i, j-1]
                 end
                 if left in digits
-                    num = read_number(grid, i, j-1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i, j-1) )
+                    # print("$num\n")
+                    # total += num
                 end
                 if j < N_DIM_2
                     right = grid[i, j+1]
                 end
                 if right in digits
-                    num = read_number(grid, i, j+1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i, j+1))
+                    # print("$num\n")
+                    # total += num
                 end
                 if i < N_DIM_1 && j > 1
                     lower_left = grid[i+1, j-1]
                 end
                 if lower_left in digits
-                    num = read_number(grid, i+1, j-1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i+1, j-1))
+                    # print("$num\n")
+                    # total += num
                 end 
                 if i < N_DIM_1
                     bottom = grid[i+1, j]
                 end
                 if bottom in digits
-                    num = read_number(grid, i+1, j)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i+1, j))
+                    # print("$num\n")
+                    # total += num
                 end
                 if i < N_DIM_1 && j < N_DIM_2
                     lower_right = grid[i+1, j+1]
                 end
                 if lower_right in digits
-                    num = read_number(grid, i+1, j+1)
-                    print("$num\n")
-                    total += num
+                    match_count += 1
+                    append!(tentative_digit_list, read_number(grid, i+1, j+1))
+                    # print("$num\n")
+                    # total += num
                 end 
-
+                # check if only 2 matches
+                if match_count == 2
+                    total += reduce(*, tentative_digit_list)
+                end
             end
         end
     end
     return total
 end
 
-solve_part_1()
+
+solve_part_2()
+
